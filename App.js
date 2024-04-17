6b875ce78266bdf396253ac9a76fa70c2b1f3ebf
Caike <125682254+CaikeHenry007@users.noreply.github.com>
2024-04-17 12:09:05 -0300

    ok

--- App.js
+++ App.js
@@ -1,31 +1,76 @@
-import { View, Text, FlatList } from "react-native";
-import { styles } from "./src/styles/Style";
+import { View, Text, FlatList, ScrollView } from "react-native";
+import styles from "./src/style/Styles";
 
 export default function App() {
-  const data = [
-    { id: "1", backgroundColor: "red" },
-    { id: "2", backgroundColor: "pink" },
-    { id: "3", backgroundColor: "blue" },
-    { id: "4", backgroundColor: "yellow" },
-    { id: "5", backgroundColor: "green" },
+  const ligas = [
+    { id: "1", backgroundColor: "red", title: "Premier League" },
+    { id: "2", backgroundColor: "pink", title: "La Liga" },
+    { id: "3", backgroundColor: "blue", title: "Serie A" },
+    { id: "4", backgroundColor: "yellow", title: "Ligue One" },
+    { id: "5", backgroundColor: "green", title: "Bundesliga" },
+  ];
+
+  const times = [
+    { id: "1", backgroundColor: "red", title: "Liverpool" },
+    { id: "2", backgroundColor: "pink", title: "Real Madrid" },
+    { id: "3", backgroundColor: "blue", title: "Milan" },
+    { id: "4", backgroundColor: "yellow", title: "Paris Saint-Germain" },
+    { id: "5", backgroundColor: "green", title: "Bayern Lewerkusen" },
+  ];
+
+  const jogadores = [
+    { id: "1", backgroundColor: "red", title: "Van Dijk" },
+    { id: "2", backgroundColor: "pink", title: "Bellingham" },
+    { id: "3", backgroundColor: "blue", title: "Rafael Leão" },
+    { id: "4", backgroundColor: "yellow", title: "Mbappé" },
+    { id: "5", backgroundColor: "green", title: "Witz" },
   ];
 
   // Renderiza os Componentes Views(box) com seu respectivo Texto.
   // Recebe nosso array de objeto, nomeado como data!
-  const renderItem = ({ item }) => (
+  const renderLiga = ({ item }) => (
     <View style={[styles.box, { backgroundColor: item.backgroundColor }]}>
-      <Text style={styles.txt}>React Native!</Text>
+      <Text style={styles.txt}>{item.title}</Text>
+    </View>
+  );
+
+  const renderTime = ({ item }) => (
+    <View style={[styles.box, { backgroundColor: item.backgroundColor }]}>
+      <Text style={styles.txt}>{item.title}</Text>
+    </View>
+  );
+
+  const renderJogador = ({ item }) => (
+    <View style={[styles.box, { backgroundColor: item.backgroundColor }]}>
+      <Text style={styles.txt}>{item.title}</Text>
     </View>
   );
 
   return (
     <View style={styles.container}>
       <View style={styles.header}></View>
-      <FlatList
-        data={data} // Ele está recebendo todos os objetos do nosso array data
-        renderItem={renderItem} // Chama a função para renderizar as Views box
-        keyExtractor={(data) => data.id} // Extrair o id do objeto
-      />
+      <ScrollView>
+        <FlatList
+          data={ligas} // Ele está recebendo todos os objetos do nosso array data
+          renderItem={renderLiga} // Chama a função para renderizar as Views box
+          keyExtractor={(ligas) => ligas.id} // Extrair o id do objeto
+          horizontal={true}
+        />
+
+        <FlatList
+          data={times} // Ele está recebendo todos os objetos do nosso array data
+          renderItem={renderTime} // Chama a função para renderizar as Views box
+          keyExtractor={(times) => times.id} // Extrair o id do objeto
+          horizontal={true}
+        />
+
+        <FlatList
+          data={jogadores} // Ele está recebendo todos os objetos do nosso array data
+          renderItem={renderJogador} // Chama a função para renderizar as Views box
+          keyExtractor={(jogadores) => jogadores.id} // Extrair o id do objeto
+          horizontal={true}
+        />
+      </ScrollView>
       <View style={styles.footer}></View>
     </View>
   );
--- src/style/Styles.js
+++ src/style/Styles.js
@@ -2,10 +2,30 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1, // Preencha todo o espaço que esta vazio/sobrando
+  },
+
+  header: {
+    height: 80, // Define a altura do HEADER
+    backgroundColor: "black", // Define a cor de fundo
+  },
+
+  box: {
+    height: 350, // Define a altura dos componentes box
+    margin: 5, // Espaçamento externo
+    width: 1000, // Define a largura dos componentes box
+    alignItems: "center", // Centraliza o que estiver dentro da BOX
+    justifyContent: "center", // Centraliza o que estiver dentro da BOX
+  },
+
+  txt: {
+    fontSize: 30, // Define o tamanho da fonte
+    fontWeight: "bold", // Define a " largura " da fonte
+  },
+
+  footer: {
+    height: 100, // Define a altura do componente Footer
+    backgroundColor: "black", // Define a cor de fundo
   },
 });
 
